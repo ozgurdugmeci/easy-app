17715b7677eeca5f3cdc4ce82d4844f8c8e7ef8f
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2021-05-13 12:06:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ elif yan_sayfa_secenek == 'Application Guide' :
 
 
 
-takip= f '<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101316655);</script> <script async src="//static.getclicky.com/js"></script> <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101316655ns.gif" /></p></noscript> </body> </html>'
+takip= f '<gtml> <body> <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101316655);</script> <script async src="//static.getclicky.com/js"></script> <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101316655ns.gif" /></p></noscript> </body> </html>'
 st.markdown(takip, unsafe_allow_html=True)    
     
     
