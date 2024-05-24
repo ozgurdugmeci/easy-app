88affc96af27b42605e03adc9b43516bfdb964a1
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-05-24 11:04:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -675,7 +675,7 @@ elif yan_sayfa_secenek == 'Direct Excel Upload' :
   df_tutarlk2=df_tutarlk2.astype(str)
   df_tutarlk2['Ratio']='%' + df_tutarlk2['Ratio']
   #df_tutarlk2=df_tutarlk2.sort_values(by='Ratio', ascending=False)
-  #st.dataframe(df_tutarlk2)
+  st.dataframe(df_tutarlk2)
   'Predictability Analysis shows the quality of inventory management. The higher percentage of the "Predictable Sales" ratio indicates the good quality of the inventory management.'
   #download_data
   st.info('B- Inventory Planner')
@@ -697,14 +697,14 @@ elif yan_sayfa_secenek == 'Direct Excel Upload' :
   df_analiz_show['Predicted_Sales']=df_analiz_show['Predicted_Sales'].astype(int)
   df_analiz_show=df_analiz_show.reset_index(drop=True)
   df_analiz_show2=df_analiz_show.astype(str)
-  #st.dataframe(df_analiz_show2)
+  st.dataframe(df_analiz_show2)
     
   if len(df_sfr)>0:
    st.info('C- Zero Sales') 
    'The table shows the products which have no sales in last 80 days.'
    df_sfr=df_sfr.reset_index(drop=True)
    df_sfr2=df_sfr.astype(str).copy()
-   #st.dataframe(df_sfr2)   
+   st.dataframe(df_sfr2)   
   #except:
   #'Excel dosya sütunlarını kontrol edin.'
  
