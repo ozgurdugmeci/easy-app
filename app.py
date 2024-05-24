ecf69e8d99ca6cdaacb57dd5244eb0965d6a4b18
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-05-24 10:37:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -682,11 +682,11 @@ elif yan_sayfa_secenek == 'Direct Excel Upload' :
   'Stock_Cover : The number of days until a product will be out of stock with the predicted sales speed.'
   'Predicted_Sales : Estimated 30-day sale values '
    
-  isim= 'Analsed_Data.csv'
-  indir = df_analiz_download.to_csv(index=False)
-  b64 = base64.b64encode(indir.encode(encoding='ISO-8859-1')).decode(encoding='ISO-8859-1')  # some strings
-  linko_final= f'<a href="data:file/csv;base64,{b64}" download={isim}>Download Analysed Data</a>'
-  st.markdown(linko_final, unsafe_allow_html=True)  
+  #isim= 'Analsed_Data.csv'
+  #indir = df_analiz_download.to_csv(index=False)
+  #b64 = base64.b64encode(indir.encode(encoding='ISO-8859-1')).decode(encoding='ISO-8859-1')  # some strings
+  #linko_final= f'<a href="data:file/csv;base64,{b64}" download={isim}>Download Analysed Data</a>'
+  #st.markdown(linko_final, unsafe_allow_html=True)  
    
   df_analiz_show= df_analiz[['Product','Category','Sub-Category','Inventory','Label','Stock_Cover', 'Predicted_Sales','Sales21','Sales42','Sales63','Sales84']].copy()
    
