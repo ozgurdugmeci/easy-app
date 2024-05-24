177ba9824c086aa33d630f086f889526280cbca1
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-05-24 09:50:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -546,7 +546,7 @@ elif yan_sayfa_secenek == 'Direct Excel Upload' :
    df['Sub-Category'] = df['Sub-Category'].astype('str')
    df_dummx= df.copy() 
    df_dummx= df_dummx.astype(str)
-     
+   'hey'  
    df['Product'].fillna('-',inplace=True)
    df['Category'].fillna('-',inplace=True)
    df['Sub-Category'].fillna('-',inplace=True)
