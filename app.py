3b6c9ed628bc29fe1760e27ae132c9b89d3693bf
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-06-05 12:06:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -737,7 +737,8 @@ elif yan_sayfa_secenek == 'Application Guide' :
  
  urly= f'<a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=kzYUHItdyOQ">Click to watch how to use easy inventory planner app.</a>'
  st.markdown(urly,unsafe_allow_html=True)
-
+ urly_download_new= f'<a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=3zrxMl0cueQ">Click to watch how to use one-file upload.</a>'
+ st.markdown(urly_download_new,unsafe_allow_html=True)
  htp0='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/model3.jpg'
  #image0 = Image.open(htp6)
  st.image(htp0, caption= 'Model', width=800)
