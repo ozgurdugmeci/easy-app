9c998de6d3825a140df63929116315780aaee509
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-08-15 11:45:09 +0300

    Update uc.py

--- pages/uc.py
+++ pages/uc.py
@@ -1,1 +1,9 @@
 
+st.title('Contact With Us')
+mail_b= f'<p>Your feed back is very important for us. Write about your user experience. Give us kudos! Ask questions. Inform us\
+about the errors you encountered. Tell about new ideas. You can reach us by clicking the link below. </p>'
+ 
+info_link= f'<a target="_blank" rel="noopener noreferrer" href="https://ozgur-dugmeci.medium.com/easy-inventory-planner-c1e5fc4aa0e">Click here to leave a comment.</a>'
+st.markdown(mail_b,unsafe_allow_html=True)
+st.markdown(info_link,unsafe_allow_html=True)
+
