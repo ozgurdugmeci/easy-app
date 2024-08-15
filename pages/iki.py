8dc1676cfb0e681274af48763a5649e8ed1f170d
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-08-15 11:43:34 +0300

    Update iki.py

--- pages/iki.py
+++ pages/iki.py
@@ -1,1 +1,49 @@
+st.subheader('1- Predictability Analysis')
+ 
+
+htp6='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/predict_resim2.jpg'
+#image6 = Image.open(htp6)
+st.image(htp6, caption= 'Predictability Analysis', width=800)
+
+predicty= f'<p>When analyses part opens, this is the first analysis that appears on the screen. You can download this report in csv file \
+ by clicking download link in "Analyses" part. Application assigns categories for each product. And for each category\
+ model uses different calculation method to predict sales speed. This is an unsupervised machine learning model. </br>\
+ <b>Predictable Sales : </b> Products in this category have consistent sales speed in every 20 days of 80 days. Predictability Analysis shows the quality of inventory \
+ management. The higher percentage of the "Predictable Sales" ratio indicates the good quality of the inventory management. </br> <b>New Products :</b> \
+ Application also detects "New Products". Since these products are in sales for less than 41 days, taking this information into consideration, model calculates  \
+ sales speed for each product under this category. </br> <b>Unpredictable Sales :</b> Marketing activities, product availablity, stock-out situations, customer preference have effect on sales.\
+ Because of these reasons some products have inconsistent sales trends in between sales ranges. Model obtains these kind of products and labels them as\
+ "Unpredictable Sales".  Yet still model estimates a sales speed for the products in this group. </br> <b>Decreasing Sales :</b> Model detects sales decrease. And it groups \
+ these products under "Decreasing Sales" category. It can be an early warning system to check the inventory quantities and to take the necessary actions. </p>'
+ 
+st.markdown(predicty,unsafe_allow_html=True)
+
+st.subheader('2- Inventory Planner') 
+
+
+
+htp7='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/planner_resim.jpg'
+
+#htp7='gs://imajo/media/planner_resim.jpg'
+
+#image7 = Image.open(htp7)
+
+
+st.image(htp7, caption= 'Inventory Planner', width=800)
+planner= f'<p>This analysis gives the core insights to plan inventory. You can check the <b>"Stock_Cover" </b> values.  In other words, it shows the number of days until a product will be out of stock. \
+Thus necessary actions can be taken before a product quantity comes to a critical low level. <b>"Predicted_Sales_Speed"</b> is the estimated 30-day \
+ sale values.  Analysing these values with the category information can help you in taking decisions.</p>'
+
+st.markdown(planner,unsafe_allow_html=True)
+st.subheader('3- Zero Sales') 
+'The products have no sales in the last 80 days are listed here. If there are no such products, this analysis does not appear on the screen.' 
+lif yan_sayfa_secenek == 'Contact' :
+
+st.title('Contact With Us')
+mail_b= f'<p>Your feed back is very important for us. Write about your user experience. Give us kudos! Ask questions. Inform us\
+about the errors you encountered. Tell about new ideas. You can reach us by clicking the link below. </p>'
+
+info_link= f'<a target="_blank" rel="noopener noreferrer" href="https://ozgur-dugmeci.medium.com/easy-inventory-planner-c1e5fc4aa0e">Click here to leave a comment.</a>'
+st.markdown(mail_b,unsafe_allow_html=True)
+st.markdown(info_link,unsafe_allow_html=True)
 
