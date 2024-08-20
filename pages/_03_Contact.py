f5e40ad5273a1a46cb3b1e517549d335df6073f9
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-08-20 10:23:55 +0300

    Update _03_Contact.py

--- pages/_03_Contact.py
+++ pages/_03_Contact.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
-'yo'
+
 st.title('Contact With Us')
 mail_b= f'<p>Your feed back is very important for us. Write about your user experience. Give us kudos! Ask questions. Inform us\
 about the errors you encountered. Tell about new ideas. You can contact me through <u>ozgur.dugmeci@gmail.com</u> . </p>'
  
-info_link= f'<a target="_blank" rel="noopener noreferrer" href="https://ozgur-dugmeci.medium.com/easy-inventory-planner-c1e5fc4aa0e">Click here to leave a comment.</a>'
+#info_link= f'<a target="_blank" rel="noopener noreferrer" href="https://ozgur-dugmeci.medium.com/easy-inventory-planner-c1e5fc4aa0e">Click here to leave a comment.</a>'
 st.markdown(mail_b,unsafe_allow_html=True)
 #st.markdown(info_link,unsafe_allow_html=True)
 
